de74cce97ca582ae8f1240e4eb66a5d7f3ec4db9
XavierFabregat <xavi.fabregat.pous@gmail.com>
2022-06-18 13:31:46 +0200

    Added a lot of console logs throught the code

--- src/pages/dashboard-page.jsx
+++ src/pages/dashboard-page.jsx
@@ -1,81 +1,112 @@
-
-import { Storage } from "aws-amplify"
-import { useState, useEffect } from "react"
-import { PostList } from '../components/postList-comp'
-import { Menu } from "../components/menu-comp"
-import { duplicatesByMonth } from "../utils/duplicates"
-import { listUserPosts, listAllUserPosts } from "../utils/listdata"
-import { sortData } from "../utils/sort"
-import { getFriends } from "../utils/friendRequests"
+import { Storage } from 'aws-amplify';
+import { useState, useEffect } from 'react';
+import { PostList } from '../components/postList-comp';
+import { Menu } from '../components/menu-comp';
+import { duplicatesByMonth } from '../utils/duplicates';
+import { listUserPosts, listAllUserPosts } from '../utils/listdata';
+import { sortData } from '../utils/sort';
+import { getFriends } from '../utils/friendRequests';
 import { BsChevronDown } from 'react-icons/bs';
-import '../css/dashboard.css'
+import '../css/dashboard.css';
 
 //temp set to public - this needs to change and implement groups for friends !fix
 Storage.configure({ level: 'public' });
 
-export function Dashboard({user, signOut, friends, setFriends}) {
-    const [posts, setPosts] = useState([]);
-    const [allPosts, setAllPosts] = useState([]);
-    const [noPosts, setNoPosts] = useState(false);
-    const [token, setToken] = useState();
+export function Dashboard({ user, signOut, friends, setFriends }) {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [noPosts, setNoPosts] = useState(false);
+  const [token, setToken] = useState();
 
-    useEffect(() => {
-        listUserPosts(user.username).then((data) => {
-            setPosts(data.data.postByUser.items);
-            const tokenID = data.data.postByUser.nextToken;
-            setToken(tokenID);
-        })
-    }, [])
+  useEffect(() => console.log('dashboard-page.js, User: ', user), []);
 
-    useEffect(() => {
-        listAllUserPosts(user.username).then((data) => {
-            const listData = data.data.postByUser.items
-            sortData(listData);
-            setAllPosts(duplicatesByMonth(listData))
-            if (listData.length === 0) {
-                setNoPosts(true);
-            } else {
-                setNoPosts(false);
-            }
-        })
-    }, [])
+  useEffect(() => {
+    listUserPosts(user.username).then(data => {
+      console.log(
+        'Response from listUserPost @ dashboard-page.jsx line 26: ',
+        data,
+      );
+      setPosts(data.data.postByUser.items);
+      const tokenID = data.data.postByUser.nextToken;
+      setToken(tokenID);
+    });
+  }, []);
 
-    useEffect(() => {
-        getFriends(user.username).then((data) => {
-            setFriends(data)
-        })
-    }, []);
+  useEffect(() => {
+    listAllUserPosts(user.username).then(data => {
+      console.log(
+        'Response from listAllUserPosts @ dashboard-page.jsx line 38: ',
+        data,
+      );
+      const listData = data.data.postByUser.items;
+      sortData(listData);
+      setAllPosts(duplicatesByMonth(listData));
+      if (listData.length === 0) {
+        setNoPosts(true);
+      } else {
+        setNoPosts(false);
+      }
+    });
+  }, []);
 
-    async function newPage () {
-        listUserPosts(user.username, token)
-        .then((data) => {
-            if (token === null || undefined) return;
-            setPosts(prev => {
-                return [...prev, ...data.data.postByUser.items]
-            })
-            const tokenID = data.data.postByUser.nextToken;
-            setToken(tokenID);
-        })
-    }
+  useEffect(() => {
+    getFriends(user.username).then(data => {
+      console.log(
+        'Response from getFreinds @ dashboard-page.jsx line 48: ',
+        data,
+      );
+      setFriends(data);
+    });
+  }, []);
 
-    if (noPosts === true) {
-        return (
-            <div cass="container">
-            <div id="nodata">
-                <h3>No posts to display ʕ ´•̥̥̥ ᴥ•̥̥̥`ʔ</h3>
-            </div>
-            </div>
-        )
-    }
-    else {
-        return (
-            <>
-            <div class="container">
-                <PostList posts={posts} setPosts={setPosts} setAllPosts={setAllPosts}/>
-                <Menu user={user} signOut={signOut} friends={friends} setFriends={setFriends} allPosts={allPosts} posts={posts} setPosts={setPosts} token={token} setToken={setToken}/>
-                <button id="footer" onClick={newPage}><BsChevronDown /></button>
-            </div>
-            </>
-        )
-    }
+  async function newPage() {
+    listUserPosts(user.username, token).then(data => {
+      console.log(
+        'Response from listUserPosts @ dashboard-page.jsx line 65: ',
+        data,
+      );
+      if (token === null || undefined) return;
+      setPosts(prev => {
+        return [...prev, ...data.data.postByUser.items];
+      });
+      const tokenID = data.data.postByUser.nextToken;
+      setToken(tokenID);
+    });
+  }
+
+  if (noPosts === true) {
+    return (
+      <div cass="container">
+        <div id="nodata">
+          <h3>No posts to display ʕ ´•̥̥̥ ᴥ•̥̥̥`ʔ</h3>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <div class="container">
+          <PostList
+            posts={posts}
+            setPosts={setPosts}
+            setAllPosts={setAllPosts}
+          />
+          <Menu
+            user={user}
+            signOut={signOut}
+            friends={friends}
+            setFriends={setFriends}
+            allPosts={allPosts}
+            posts={posts}
+            setPosts={setPosts}
+            token={token}
+            setToken={setToken}
+          />
+          <button id="footer" onClick={newPage}>
+            <BsChevronDown />
+          </button>
+        </div>
+      </>
+    );
+  }
 }
--- src/pages/friends-page.jsx
+++ src/pages/friends-page.jsx
@@ -1,27 +1,38 @@
-import { Search } from "../components/search-comp"
-import { FriendsList } from "../components/friendsList-comp"
-import { RequestList } from "../components/requestList-comp"
-import '../css/friends.css'
-import { useState } from "react"
+import { Search } from '../components/search-comp';
+import { FriendsList } from '../components/friendsList-comp';
+import { RequestList } from '../components/requestList-comp';
+import '../css/friends.css';
+import React, { useState } from 'react';
 
-export function Friends({user, friends, setFriends}) {
+export function Friends({ user, friends, setFriends }) {
+  const [outGoing, setOutGoing] = useState([]);
+  const [incoming, setIncoming] = useState([]);
 
-    const [outGoing, setOutGoing] = useState([]);
-    const [incoming, setIncoming] = useState([]);
+  React.useEffect(() => {
+    console.log('@ friends-page.js, User: ', user);
+    console.log('@ friends-page.js, friends: ', friends);
+    console.log('@ friends-page.js, setFriends: ', setFriends);
+  }, []);
 
- return (
+  return (
     <>
-    <div id="friends">
+      <div id="friends">
         <div id="addfriends">
-            <Search user={user} outGoing={outGoing} setOutGoing={setOutGoing}/>
-            <RequestList user={user} outGoing={outGoing}
-            setOutGoing={setOutGoing} incoming={incoming}
-            setIncoming={setIncoming} friends={friends} setFriends={setFriends}/>
+          <Search user={user} outGoing={outGoing} setOutGoing={setOutGoing} />
+          <RequestList
+            user={user}
+            outGoing={outGoing}
+            setOutGoing={setOutGoing}
+            incoming={incoming}
+            setIncoming={setIncoming}
+            friends={friends}
+            setFriends={setFriends}
+          />
         </div>
         <div id="friendspagelist">
-            <FriendsList user={user} friends={friends} setFriends={setFriends}/>
+          <FriendsList user={user} friends={friends} setFriends={setFriends} />
         </div>
-    </div>
+      </div>
     </>
- )
+  );
 }
